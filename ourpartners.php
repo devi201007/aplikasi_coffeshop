ac20248eba34fcab39c48e06ed012217f84f1d57
unknown <19250785@bsi.sc.id>
2026-06-30 14:13:27 +0700

    first commit

--- ourpartners.php
+++ ourpartners.php
@@ -64,7 +64,6 @@
     <a href="home.php">Beranda</a>
     <a href="about.php">About Us</a>
     <a href="contact.php">Contact</a>
-    <a href="order.php">Order</a>
     <a href="faq.php">FAQ</a>
     <a href="membership.php">Membership</a>
     <a href="gallerycaffe.php">Gallery</a>
