abb674f8f43254a81b95668480754d63b9fa4fbd
Aulia Maharani <berrybizz02@gmail.com>
2026-07-05 09:21:33 +0700

    feat: add new faq progress part 18

--- database_import.sql
+++ database_import.sql
@@ -168,3 +168,4 @@ INSERT INTO `reservasi` (`nama_pelanggan`, `email`, `telepon`, `tanggal`, `jam`,
 --progress nambahin data 15
 --progress nambahin data 16
 --progress nambahin data 17
+--progress nambahin data 18
